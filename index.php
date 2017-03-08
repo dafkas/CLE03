67d62c4dbb4043820dd2c11c0ff0c9acd16c8277
marvin holleman <marvin_blabla@145-24-174-107.wlan.hro.nl>
2017-03-08 14:00:58 +0100

    Basic structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php require_once "includes/initialize.php"?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Parking-soon</title>
+    <link type="text/css" href="css/style.css"/>
+</head>
+<body>
+<h1><?= $pageTitle ?></h1>
+<?php if (isset($errors)): ?>
+    <ul class="errors">
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error; ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+</body>
+</html>
